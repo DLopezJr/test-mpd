3f7d6f154bc70442e4472592fb16622dcf4bffd3
David Lopez Jr. <42970536+DLopezJr@users.noreply.github.com>
2025-03-29 13:22:46 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ sudo apt-get install mpd mpc;
 
 mkdir -p $HOME/.config/mpd;
 
-sudo cp /etc/mpd.conf $HOME.config/mpd/mpd.conf;
+sudo cp /etc/mpd/.conf $HOME.config/mpd/mpd.conf;
+
+# config
+## music_directory ""
+## audio_output ""
+## log "verbose"
+
+sudo cp /usr/share/sounds/alsa/*.wav $HOME/
