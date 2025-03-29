bde63a52b89497eccdcd1c5166f14a122120e553
David Lopez Jr. <42970536+DLopezJr@users.noreply.github.com>
2025-03-29 13:43:53 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,22 @@ sudo apt-get install mpd mpc;
 
 mkdir -p $HOME/.config/mpd;
 
-sudo cp /etc/mpd/.conf $HOME.config/mpd/mpd.conf;
+sudo cp /etc/mpd/.conf $HOME/.config/mpd/mpd.conf;
+sudo cp /usr/share/sounds/alsa/*.wav $HOME/Music
+
+sudo chown $USER:$USER $HOME/.config/mpd/mpd.conf;
+
+mkdir -p $HOME/Music/tag_cache
+mkdir -p $HOME/Music/state
 
 # config
-## music_directory ""
-## audio_output ""
-## log "verbose"
+## music_directory "~/Music"
+## db_file "~/Music/tag_cache"
+## log_file "syslog"
+## state_file "~/Music/state
+## sticker_file COMMENT_OUT
+## log_level "verbose"
+## auto_update "yes"
 
-sudo cp /usr/share/sounds/alsa/*.wav $HOME/
+## audio_output ""
+
